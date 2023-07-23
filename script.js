8080d6af96e942e84221be481a14be30852f3412
ParsaTari <parsatari8383@gmail.com>
2023-07-23 13:42:09 +0330

    js add

---
+++ script.js
@@ -0,0 +1,26 @@
+var button = document.getElementById('enter');
+var input = document.getElementById('kosinput')
+var ul = document.querySelector('ul')
+
+button.addEventListener("click", function (){
+	if (input.value.length > 0){
+			var li = document.createElement("li")
+			li.appendChild(document.createTextNode(input.value));
+			ul.appendChild(li)
+			input.value = ""
+	}
+		
+
+}); 
+
+
+input.addEventListener("keypress", function (event){
+	if (input.value.length > 0 && event.which === 13){
+			var li = document.createElement("li")
+			li.appendChild(document.createTextNode(input.value));
+			ul.appendChild(li)
+			input.value = ""
+	}
+		
+
+}); 
